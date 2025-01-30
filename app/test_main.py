86e5767fb71fa0c70b77836c3235995f95e60599
Tymofii Khlistik <tima0323199@gmail.com>
2025-01-30 23:52:51 +0200

    Solution v.4

--- app/test_main.py
+++ app/test_main.py
@@ -1,41 +1,23 @@
 from app.main import get_coin_combination
 
-
-def test_coin_combination_with_one_penny_coin():
-    cents = 1
-    assert get_coin_combination(cents) == [1, 0, 0, 0]
+import pytest
 
 
-def test_coin_combination_with_one_penny_one_nickel():
-    cents = 5
-    assert get_coin_combination(cents) == [0, 1, 0, 0]
-
-
-def test_coin_combination_with_one_dime():
-    cents = 10
-    assert get_coin_combination(cents) == [0, 0, 1, 0]
-
-
-def test_coin_combination_with_one_quarters():
-    cents = 25
-    assert get_coin_combination(cents) == [0, 0, 0, 1]
-
-
-def test_coin_combination_with_two_quarters():
-    cents = 50
-    assert get_coin_combination(cents) == [0, 0, 0, 2]
-
-
-def test_coin_combination_with_all_coin():
-    cents = 116
-    assert get_coin_combination(cents) == [1, 1, 1, 4]
-
-
-def test_coin_combination_with_zero_cents():
-    cents = 0
-    assert get_coin_combination(cents) == [0, 0, 0, 0]
-
-
-def test_coin_combination_with_negative_cents():
-    cents = -10
-    assert get_coin_combination(cents) == [0, 0, 0, 0]
+class TestCoinCombination:
+    @pytest.mark.parametrize(
+        "value,expected_result",
+        [
+            (1, [1, 0, 0, 0]),
+            (6, [1, 1, 0, 0]),
+            (17, [2, 1, 1, 0]),
+            (50, [0, 0, 0, 2]),
+            (0, [0, 0, 0, 0]),
+            (116, [1, 1, 1, 4])
+        ]
+    )
+    def test_combination(
+            self,
+            value: int,
+            expected_result: list,
+    ) -> None:
+        assert get_coin_combination(value) == expected_result
