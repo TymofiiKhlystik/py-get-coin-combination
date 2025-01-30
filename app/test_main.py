c73f2bff1550cb85b2a2f8cdd3f30c238a2e8ccf
Tymofii Khlistik <tima0323199@gmail.com>
2025-01-30 23:41:17 +0200

    Solution 2

--- app/test_main.py
+++ app/test_main.py
@@ -7,15 +7,35 @@ def test_coin_combination_with_one_penny_coin():
 
 
 def test_coin_combination_with_one_penny_one_nickel():
-    cents = 6
-    assert get_coin_combination(cents) == [1, 1, 0, 0]
+    cents = 5
+    assert get_coin_combination(cents) == [0, 1, 0, 0]
 
 
-def test_coin_combination_with_two_penny_one_nickel_one_dime():
-    cents = 17
-    assert get_coin_combination(cents) == [2, 1, 1, 0]
+def test_coin_combination_with_one_dime():
+    cents = 10
+    assert get_coin_combination(cents) == [0, 0, 1, 0]
+
+
+def test_coin_combination_with_one_quarters():
+    cents = 25
+    assert get_coin_combination(cents) == [0, 0, 0, 1]
 
 
 def test_coin_combination_with_two_quarters():
     cents = 50
     assert get_coin_combination(cents) == [0, 0, 0, 2]
+
+
+def test_coin_combination_with_all_coin():
+    cents = 116
+    assert get_coin_combination(cents) == [1, 1, 1, 4]
+
+
+def test_coin_combination_with_zero_cents():
+    cents = 0
+    assert get_coin_combination(cents) == [0, 0, 0, 0]
+
+
+def test_coin_combination_with_negative_cents():
+    cents = -10
+    assert get_coin_combination(cents) == "Invalid cents"
